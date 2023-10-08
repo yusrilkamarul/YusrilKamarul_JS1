6205b0b8910c36a841fb097bf939bdd657f321ce
yusrilkamarul <yusrilrambe5@gmail.com>
2023-10-08 07:59:14 +0700

    first commit

---
+++ tugas.js
@@ -0,0 +1,122 @@
+
+//var nilai = 85;
+
+// Menggunakan pernyataan if untuk mengecek kondisi
+//if (nilai >= 75) {
+  //console.log("Anda lulus.");
+//} else {
+ // console.log("Anda gagal.");
+//}
+
+// Contoh nested if
+//var nilai2 = 85;
+
+//if (nilai2 >= 70) {
+ // console.log("Anda lulus");
+  //if (nilai2 >= 90) {
+   // console.log("Anda mendapatkan nilai A.");
+  //} else if (nilai2 >= 80) {
+   // console.log("Anda mendapatkan nilai B.");
+ // } else {
+    //console.log("Anda mendapatkan nilai C.");
+ // }
+//} else {
+  //console.log("Anda gagal.");
+//}
+
+
+//  switch case
+
+// Mendefinisikan variabel untuk hari dalam seminggu (1 hingga 7)
+var hari = 7;
+
+// Menggunakan  switch untuk mengevaluasi variabel 'hari'
+//switch (hari) {
+  //case 1:
+   // console.log("Hari ini adalah Minggu.");
+   // break;
+ // case 2:
+    //console.log("Hari ini adalah Senin.");
+    //break;
+  //case 3:
+  //  console.log("Hari ini adalah Selasa.");
+   // break;
+  //case 4:
+  //  console.log("Hari ini adalah Rabu.");
+   // break;
+ // case 5:
+  //  console.log("Hari ini adalah Kamis.");
+    //break;
+ // case 6:
+  //  console.log("Hari ini adalah Jumat.");
+  //  break;
+  //case 7:
+   // console.log("Hari ini adalah Sabtu.");
+   // break;
+  //default:
+  //  console.log("Input tidak valid.");
+   // break;
+//}
+
+
+
+// for statment
+// Perulangan for untuk mengiterasi melalui sebuah array
+
+//var buah = ["Alpukat", "anggur", "rambutan", "Mangga"];
+
+///console.log("Buah yang tersedia:");
+
+//for (var k = 0; k < buah.length; k++) {
+  //console.log(buah[k]);
+//}
+
+
+
+   // while
+// Inisialisasi variabel
+//var angka = 1;
+
+// Perulangan while hingga angka mencapai 5
+//while (angka <= 5) {
+ // console.log("Angka ke-" + angka);
+  //angka++;
+//}
+
+
+//  do while untuk menghitung jumlah bilangan bulat positif hingga nilai tertentu
+//var nilaiAkhir = 5;
+//var jumlah = 0;
+//var bilangan = 1;
+
+//do {
+  //jumlah += bilangan;
+  //bilangan++;
+//} while (bilangan <= nilaiAkhir);
+//console.log("Jumlah bilangan bulat positif hingga " + nilaiAkhir + " adalah " + jumlah);
+
+
+
+//  function untuk mencari nama dalam daftar
+
+// Daftar nama
+//var daftarNama = ["John", "Sarah", "Michael", "Emma", "David"];
+
+// Fungsi untuk mencari nama dalam daftar
+//function cariNama(nama, daftar) {
+ // for (var i = 0; i < daftar.length; i++) {
+  //  if (daftar[i] === nama) {
+  //    return nama + " ditemukan dalam daftar.";
+  //  }
+  //}
+ // return nama + " tidak ditemukan dalam daftar.";
+//}
+
+// Memanggil fungsi untuk mencari nama
+//var namaCari = "Emma";
+//var hasilPencarian = cariNama(namaCari, daftarNama);
+//console.log(hasilPencarian);
+
+//namaCari = "Alice";
+//hasilPencarian = cariNama(namaCari, daftarNama);
+//console.log(hasilPencarian);
